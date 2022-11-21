ec28a6f8ed41e78bbbc23826c515b142fd1129bc
Michael Loukeris <michael.loukeris@icloud.com>
2022-11-21 19:29:50 +0200

    Add optiplant to the docker compose (kit4sme) architecture

---
+++ compose/settings.py
@@ -0,0 +1,25 @@
+# see docker-compose
+ORION_EXTERNAL_BASE_URL = "http://localhost:1026"
+TENANT = "csic"
+ROUGHNATOR_INTERNAL_BASE_URL = "http://roughnator:8000"
+OPTIPLANT_INTERNAL_BASE_URL = "http://optiplant:5000"
+QUANTUMLEAP_INTERNAL_BASE_URL = "http://quantumleap:8668"
+
+ROUGHNATOR_SUB = {
+    "description": "Notify Roughnator of changes to any entity.",
+    "subject": {"entities": [{"idPattern": ".*"}]},
+    "notification": {"http": {"url": f"{ROUGHNATOR_INTERNAL_BASE_URL}/updates"}},
+}
+
+OPTIPLANT_SUB = {
+    "description": "Notify Optiplant of changes to any entity.",
+    "subject": {"entities": [{"idPattern": ".*"}]},
+    "notification": {"http": {"url": f"{OPTIPLANT_INTERNAL_BASE_URL}/prediction"}},
+}
+
+
+QUANTUMLEAP_SUB = {
+    "description": "Notify QuantumLeap of changes to any entity.",
+    "subject": {"entities": [{"idPattern": ".*"}]},
+    "notification": {"http": {"url": f"{QUANTUMLEAP_INTERNAL_BASE_URL}/v2/notify"}},
+}
